0a6d9fdfd262cd2f63106a6339b7ae9cdea54b97
Timothy Lee Styer <tlstyer@gmail.com>
2014-04-30 00:25:49 +0700

    python 3.4. use autobahn python's asyncio version.

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-for y in xrange(0, 9):
+for y in range(0, 9):
     board_body_row = '<tr class="row-' + str(y) + '">'
-    for x in xrange(0, 12):
+    for x in range(0, 12):
         board_body_row += '<td class="col-' + str(x) + '">' + str(x + 1) + chr(y + 65) + '</td>'
     board_body_row += '</tr>'
-    print board_body_row
+    print(board_body_row)
--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 import json
 import sys
 import random
-from twisted.internet import reactor
-from twisted.python import log
-from autobahn.websocket import WebSocketServerFactory, \
-    WebSocketServerProtocol, \
-    listenWS
+import asyncio
+from autobahn.asyncio.websocket import WebSocketServerFactory, WebSocketServerProtocol
 
 
 json_encoder = json.JSONEncoder(separators=(',', ':'))
@@ -30,13 +27,13 @@ random_game = None
 
 class AcquireServerProtocol(WebSocketServerProtocol):
     def onOpen(self):
-        print 'connected:', self.peerstr
-        clients[self.peerstr] = self
-        self.sendMessage(json_encoder.encode([['set-board', random_game.game_board.row_to_col_to_board_type]]))
+        print('connected:', self.peer)
+        clients[self.peer] = self
+        self.sendMessage(json_encoder.encode([['set-board', random_game.game_board.row_to_col_to_board_type]]).encode('utf-8'))
 
     def onClose(self, wasClean, code, reason):
-        print 'disconnected:', self.peerstr
-        del clients[self.peerstr]
+        print('disconnected:', self.peer)
+        del clients[self.peer]
 
     def onMessage(self, msg, binary):
         pass
@@ -46,7 +43,7 @@ class GameBoard:
     row_to_col_to_board_type = None
 
     def __init__(self):
-        self.row_to_col_to_board_type = [['none' for x in xrange(0, 12)] for y in xrange(0, 9)]
+        self.row_to_col_to_board_type = [['none' for x in range(0, 12)] for y in range(0, 9)]
 
     def set_cell(self, row, col, board_type):
         self.row_to_col_to_board_type[row][col] = board_type
@@ -56,7 +53,7 @@ class TileBag:
     tiles = None
 
     def __init__(self):
-        tiles = [(y, x) for y in xrange(0, 9) for x in xrange(0, 12)]
+        tiles = [(y, x) for y in range(0, 9) for x in range(0, 12)]
         random.shuffle(tiles)
         self.tiles = tiles
 
@@ -83,7 +80,7 @@ random_game = Game()
 
 def send_random_messages():
     messages = []
-    for i in xrange(1, random.randrange(2, 5)):
+    for i in range(1, random.randrange(2, 5)):
         tile = random_game.tile_bag.get_tile()
         if tile is not None:
             board_type = random.choice(board_types)
@@ -92,27 +89,33 @@ def send_random_messages():
 
     if len(messages) > 0:
         messages_json = json_encoder.encode(messages)
-        print messages_json
-        for client in clients.itervalues():
-            client.sendMessage(messages_json)
+        print(messages_json)
+        messages_json_bytes = messages_json.encode('utf-8')
+        for client in clients.values():
+            client.sendMessage(messages_json_bytes)
 
-        reactor.callLater(.5, send_random_messages)
+        asyncio.get_event_loop().call_later(.5, send_random_messages)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'debug':
-        log.startLogging(sys.stdout)
         debug = True
     else:
         debug = False
 
-    factory = WebSocketServerFactory("ws://localhost:9000",
-                                     debug=debug,
-                                     debugCodePaths=debug)
-
+    factory = WebSocketServerFactory('ws://localhost:9000', debug=debug)
     factory.protocol = AcquireServerProtocol
-    listenWS(factory)
 
-    reactor.callLater(3, send_random_messages)
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, '127.0.0.1', 9000)
+    server = loop.run_until_complete(coro)
 
-    reactor.run()
+    loop.call_later(3, send_random_messages)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
