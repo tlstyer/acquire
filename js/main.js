9815b34c915d2d5c8d86b8a65e9b147dc305f8f1
Timothy Lee Styer <tlstyer@gmail.com>
2014-04-30 02:11:16 +0700

    format main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 requirejs.config({
-    'baseUrl': 'js',
-    'paths': {
-      'jquery': 'http://code.jquery.com/jquery-2.1.0.min'
-    }
+	'baseUrl': 'js',
+	'paths': {
+		'jquery': 'http://code.jquery.com/jquery-2.1.0.min'
+	}
 });
 
 requirejs(['app']);
