5c14abcd02c3ba2edd92a8edd83a3b07d818bff3
Timothy Lee Styer <tlstyer@gmail.com>
2014-06-18 08:09:34 +0700

    in the client, keep track of game_id_to_player_data

--- js/common_data.js
+++ js/common_data.js
@@ -2,7 +2,8 @@ define(function(require) {
 	var pubsub = require('pubsub'),
 		data = {
 			client_id: null,
-			client_id_to_username: {}
+			client_id_to_username: {},
+			game_id_to_player_data: {}
 		};
 
 	var setClientId = function(client_id) {
@@ -14,10 +15,32 @@ define(function(require) {
 			} else {
 				data.client_id_to_username[client_id] = username;
 			}
+		},
+		createGame = function(game_id) {
+			data.game_id_to_player_data[game_id] = {};
+		},
+		setGamePlayerUsername = function(game_id, player_id, username) {
+			data.game_id_to_player_data[game_id][player_id] = {
+				username: username,
+				client_id: null
+			};
+		},
+		setGamePlayerClientId = function(game_id, player_id, client_id) {
+			if (client_id === null) {
+				data.game_id_to_player_data[game_id][player_id].client_id = null;
+			} else {
+				data.game_id_to_player_data[game_id][player_id] = {
+					username: data.client_id_to_username[client_id],
+					client_id: client_id
+				};
+			}
 		};
 
 	pubsub.subscribe('server-SetClientId', setClientId);
 	pubsub.subscribe('server-SetClientIdToUsername', setClientIdToUsername);
+	pubsub.subscribe('server-CreateGame', createGame);
+	pubsub.subscribe('server-SetGamePlayerUsername', setGamePlayerUsername);
+	pubsub.subscribe('server-SetGamePlayerClientId', setGamePlayerClientId);
 
 	return data;
 });
