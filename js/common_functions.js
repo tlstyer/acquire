a7d9431e15541528fdf0ed16a206024735774a5f
Timothy Lee Styer <tlstyer@gmail.com>
2014-06-24 11:08:19 +0700

    remove getUsernameElement()

--- js/common_functions.js
+++ js/common_functions.js
@@ -1,11 +1,7 @@
 define(function(require) {
-	var $ = require('jquery'),
-		common_data = require('common_data');
+	var $ = require('jquery');
 
 	return {
-		getUsernameElement: function(client_id) {
-			return $('<span class="username"/>').text(common_data.client_id_to_data[client_id].username);
-		},
 		getHyphenatedStringFromEnumName: function(enum_name) {
 			return enum_name.replace(/([A-Z])/g, function($1) {
 				return '-' + $1.toLowerCase();
