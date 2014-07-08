6df4cac9832ca4e18252ba40cde36c4d2788f44b
Timothy Lee Styer <tlstyer@gmail.com>
2014-07-08 09:49:41 +0700

    include python files in the distribution

--- generate_distribution_files.sh
+++ generate_distribution_files.sh
@@ -4,21 +4,27 @@ TIMESTAMP=$(date +%s)
 
 mkdir -p dist
 rm -rf dist/*
-mkdir dist/static
+mkdir -p dist/web/static
 
 # index.html
 sed "s/<link rel=\"stylesheet\" href=\"css\/main.css\">/<link rel=\"stylesheet\" href=\"static\/${TIMESTAMP}.css\">/" index.html | \
 sed "s/<script data-main=\"js\/main\" src=\"node_modules\/requirejs\/require.js\"><\/script>/<script src=\"static\/${TIMESTAMP}.js\"><\/script>/" | \
 ./node_modules/html-minifier/cli.js \
 	--remove-comments --collapse-whitespace --conservative-collapse --collapse-boolean-attributes --remove-attribute-quotes --remove-redundant-attributes --remove-optional-tags \
-	-o dist/index.html
+	-o dist/web/index.html
 
 # main.css
-./node_modules/requirejs/bin/r.js -o cssIn=css/main.css out=dist/main.css
-./node_modules/clean-css/bin/cleancss -o dist/static/${TIMESTAMP}.css dist/main.css
-rm dist/main.css
+./node_modules/requirejs/bin/r.js -o cssIn=css/main.css out=dist/web/main.css
+./node_modules/clean-css/bin/cleancss -o dist/web/static/${TIMESTAMP}.css dist/web/main.css
+rm dist/web/main.css
 
 # main.js
 cd js
-../node_modules/requirejs/bin/r.js -o baseUrl=. name=../node_modules/almond/almond.js wrap=true include=main out=../dist/static/${TIMESTAMP}.js
+../node_modules/requirejs/bin/r.js -o baseUrl=. name=../node_modules/almond/almond.js wrap=true include=main out=../dist/web/static/${TIMESTAMP}.js
 cd ..
+
+# enums.py
+cp enums.py dist
+
+# server.py
+cp server.py dist
