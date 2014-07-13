91659c72ddd1534b1aba3ab115e54e30bca2b2f9
Timothy Lee Styer <tlstyer@gmail.com>
2014-07-13 22:02:25 +0700

    put username in page title. remove it when logged out.

--- js/notification.js
+++ js/notification.js
@@ -1,11 +1,12 @@
 define(function(require) {
-	var title = 'Acquire',
-		title_prefix = '!!! YOUR TURN !!! ',
+	var common_data = require('common_data'),
+		pubsub = require('pubsub'),
+		title = '',
 		interval = null,
 		showing_title_prefix = false,
 		intervalCallback = function() {
 			showing_title_prefix = !showing_title_prefix;
-			document.title = (showing_title_prefix ? title_prefix : '') + title;
+			document.title = (showing_title_prefix ? '!!! YOUR TURN !!! ' : '') + title;
 		},
 		turnOn = function() {
 			if (interval === null) {
@@ -20,7 +21,21 @@ define(function(require) {
 				showing_title_prefix = false;
 				document.title = title;
 			}
+		},
+		onClientSetClientData = function() {
+			title = 'Acquire - ' + common_data.client_id_to_data[common_data.client_id].username;
+			showing_title_prefix = !showing_title_prefix;
+			intervalCallback();
+		},
+		resetTitle = function() {
+			title = 'Acquire';
+			showing_title_prefix = !showing_title_prefix;
+			intervalCallback();
 		};
+
+	pubsub.subscribe('client-SetClientData', onClientSetClientData);
+	pubsub.subscribe('network-Close', resetTitle);
+	pubsub.subscribe('network-Error', resetTitle);
 
 	return {
 		turnOn: turnOn,
