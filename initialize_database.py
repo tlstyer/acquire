5ee840858cb1fa6bf736b198ab0ceb769bae5a5d
Timothy Lee Styer <tlstyer@gmail.com>
2014-08-18 13:49:16 +0700

    put module code inside a function

--- initialize_database.py
+++ initialize_database.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3.4m
 
-import subprocess
 import orm
-from sqlalchemy.orm import sessionmaker
+import sqlalchemy.orm
+import subprocess
 
-subprocess.call(['mysql', '-u', 'root', '-proot', '-e', 'drop schema if exists `acquire`; create schema `acquire` default character set utf8 collate utf8_bin;'])
 
-orm.Base.metadata.create_all(orm.engine)
+def main():
+    subprocess.call(['mysql', '-u', 'root', '-proot', '-e', 'drop schema if exists `acquire`; create schema `acquire` default character set utf8 collate utf8_bin;'])
 
-Session = sessionmaker(bind=orm.engine)
-session = Session()
+    orm.Base.metadata.create_all(orm.engine)
 
-session.add(orm.GameMode(name='Singles'))
-session.add(orm.GameMode(name='Teams'))
+    session = sqlalchemy.orm.sessionmaker(bind=orm.engine)()
 
-session.add(orm.GameState(name='Starting'))
-session.add(orm.GameState(name='StartingFull'))
-session.add(orm.GameState(name='InProgress'))
-session.add(orm.GameState(name='Completed'))
+    session.add(orm.GameMode(name='Singles'))
+    session.add(orm.GameMode(name='Teams'))
 
-session.add(orm.RatingType(name='Singles2'))
-session.add(orm.RatingType(name='Singles3'))
-session.add(orm.RatingType(name='Singles4'))
-session.add(orm.RatingType(name='Teams'))
+    session.add(orm.GameState(name='Starting'))
+    session.add(orm.GameState(name='StartingFull'))
+    session.add(orm.GameState(name='InProgress'))
+    session.add(orm.GameState(name='Completed'))
 
-session.commit()
+    session.add(orm.RatingType(name='Singles2'))
+    session.add(orm.RatingType(name='Singles3'))
+    session.add(orm.RatingType(name='Singles4'))
+    session.add(orm.RatingType(name='Teams'))
+
+    session.commit()
+
+
+if __name__ == '__main__':
+    main()
