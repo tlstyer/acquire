d9013b6cbb8adee56523f87a4d8818c86539762a
Timothy Lee Styer <tlstyer@gmail.com>
2014-11-02 19:29:52 +0700

    polyfill for Array.prototype.indexOf

--- js/polyfills.js
+++ js/polyfills.js
@@ -1,10 +1,39 @@
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
 if (!String.prototype.trim) {
 	(function() {
-		// Make sure we trim BOM and NBSP
 		var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
 		String.prototype.trim = function() {
 			return this.replace(rtrim, '');
 		};
 	})();
+}
+
+// from https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function(searchElement, fromIndex) {
+		var k;
+		if (this === null) {
+			throw new TypeError('"this" is null or not defined');
+		}
+		var O = Object(this);
+		var len = O.length >>> 0;
+		if (len === 0) {
+			return -1;
+		}
+		var n = +fromIndex || 0;
+		if (Math.abs(n) === Infinity) {
+			n = 0;
+		}
+		if (n >= len) {
+			return -1;
+		}
+		k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+		while (k < len) {
+			if (k in O && O[k] === searchElement) {
+				return k;
+			}
+			k++;
+		}
+		return -1;
+	};
 }
