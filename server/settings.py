e131b64ba648883f4fefe074c7266e0434bfacae
Timothy Lee Styer <tlstyer@gmail.com>
2018-03-07 11:12:17 -0800

    my log file directories

--- server/settings.py
+++ server/settings.py
@@ -1,5 +1,4 @@
 util__get_log_file_filenames__path_prefixes = [
-    '/home/tim/acquire.tlstyer.com/live/logs_',
-    # '/home/tim/server_mirror-archive/acquire.tlstyer.com/live/logs_',
-    # '/home/tim/server_mirror/acquire.tlstyer.com/live/logs_',
+    '/home/tim/server-mirrors/acquire.tlstyer.com/game-log-history/logs_',
+    '/home/tim/server-mirrors/acquire.tlstyer.com/home/acquire.tlstyer.com/live/logs_',
 ]
