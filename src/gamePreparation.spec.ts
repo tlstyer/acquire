7ad768be622ea8cc77c935bceb86dc0552debdf9
Timothy Lee Styer <tlstyer@gmail.com>
2018-03-22 03:09:54 -0700

    test that tile bag was not in numerical order

--- src/gamePreparation.spec.ts
+++ src/gamePreparation.spec.ts
@@ -3,14 +3,15 @@ import { getNewTileBag } from './gamePreparation';
 
 describe('getNewTileBag', () => {
     it('should return a shuffled array containing all integers between 0 and 107 inclusive', () => {
-        let tileBag = getNewTileBag();
-        tileBag.sort((a, b) => (a < b ? -1 : 1));
-
-        let expected: number[] = new Array(108);
+        let tileBagInOrder: number[] = new Array(108);
         for (let i = 0; i < 108; i++) {
-            expected[i] = i;
+            tileBagInOrder[i] = i;
         }
 
-        assert.deepEqual(tileBag, expected);
+        let tileBag = getNewTileBag();
+        assert.notDeepEqual(tileBag, tileBagInOrder);
+
+        tileBag.sort((a, b) => (a < b ? -1 : 1));
+        assert.deepEqual(tileBag, tileBagInOrder);
     });
 });
