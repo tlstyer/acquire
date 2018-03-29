a7064b7b46d7b9224a2b298b330424b7f85f4d5e
Timothy Lee Styer <tlstyer@gmail.com>
2018-03-29 15:50:00 -0700

    rename gamePreparation files to helpers. prepare for more helper functions.

---
+++ src/helpers.spec.ts
@@ -0,0 +1,19 @@
+import { assert } from 'chai';
+import { getNewTileBag } from './helpers';
+
+describe('helpers', () => {
+    describe('getNewTileBag', () => {
+        it('should return a shuffled array containing all integers between 0 and 107 inclusive', () => {
+            let tileBagInOrder: number[] = new Array(108);
+            for (let i = 0; i < 108; i++) {
+                tileBagInOrder[i] = i;
+            }
+
+            let tileBag = getNewTileBag();
+            assert.notDeepEqual(tileBag, tileBagInOrder);
+
+            tileBag.sort((a, b) => (a < b ? -1 : 1));
+            assert.deepEqual(tileBag, tileBagInOrder);
+        });
+    });
+});
