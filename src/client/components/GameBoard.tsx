88f52e5a7f8d1d5ee3751af04aed427ef1e65435
Timothy Lee Styer <tlstyer@gmail.com>
2018-04-09 14:08:33 -0700

    show player's tile rack tiles on the game board

--- src/client/components/GameBoard.tsx
+++ src/client/components/GameBoard.tsx
@@ -7,12 +7,21 @@ import * as style from './GameBoard.css';
 
 const yTileNames = 'ABCDEFGHI';
 
-export const GameBoard = ({ gameBoard }: { gameBoard: List<GameBoardType> }) => {
+export function GameBoard({ gameBoard, tileRack }: { gameBoard: List<GameBoardType>; tileRack: List<number | null> }) {
+    let myTiles: { [key: number]: boolean } = {};
+    for (let tileIndex = 0; tileIndex < 6; tileIndex++) {
+        const tile = tileRack.get(tileIndex, 0);
+        if (tile !== null) {
+            myTiles[tile] = true;
+        }
+    }
+
     const rows = new Array(9);
     for (let y = 0; y < 9; y++) {
         const cells = new Array(12);
         for (let x = 0; x < 12; x++) {
-            const gameBoardType = gameBoard.get(x * 9 + y, 0);
+            const tile = x * 9 + y;
+            const gameBoardType = myTiles[tile] ? GameBoardType.IHaveThis : gameBoard.get(tile, 0);
             const tileName = `${x + 1}${yTileNames[y]}`;
             cells[x] = (
                 <td key={x} className={getCssStyleForGameBoardType(gameBoardType)}>
@@ -22,9 +31,10 @@ export const GameBoard = ({ gameBoard }: { gameBoard: List<GameBoardType> }) => 
         }
         rows[y] = <tr key={y}>{cells}</tr>;
     }
+
     return (
         <table className={style.root}>
             <tbody>{rows}</tbody>
         </table>
     );
-};
+}
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,20 +3,20 @@ import './global.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { defaultTileRack } from '../defaults';
 import { MoveData } from '../game';
 import { runGameTestFile } from '../runGameTestFile';
 import { GameBoard } from './components/GameBoard';
 
-const inputLines: string[] = require('raw-loader!../gameTestFiles/other/all tiles played').split('\n');
+const inputLines: string[] = require('raw-loader!../gameTestFiles/other/no tiles played for entire round').split('\n');
 
 async function showMoveSequence(moveData: MoveData[]) {
     for (let i = 0; i < moveData.length; i++) {
         const entry = moveData[i];
-        const gameBoard = entry.gameBoard;
 
         ReactDOM.render(
             <div style={{ width: 2 + 50 * 12, height: 2 + 50 * 9, fontSize: 50 * 2 / 5 }}>
-                <GameBoard gameBoard={gameBoard} />
+                <GameBoard gameBoard={entry.gameBoard} tileRack={entry.tileRacks.get(0, defaultTileRack)} />
             </div>,
             document.getElementById('root'),
         );
