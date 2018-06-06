f9d85986717b2017344d884bd7c4da293068625e
Timothy Lee Styer <tlstyer@gmail.com>
2018-06-06 13:12:29 -0700

    upgrade dependencies

--- src/common/gameActions/gameOver.ts
+++ src/common/gameActions/gameOver.ts
@@ -3,9 +3,11 @@ import { UserInputError } from '../error';
 import { Game } from '../game';
 import { ActionBase } from './base';
 
+const hackSoTestsWorkWithTypeScript2Dot9Dot1 = GameAction.GameOver;
+
 export class ActionGameOver extends ActionBase {
     constructor(game: Game, playerID: number) {
-        super(game, playerID, GameAction.GameOver);
+        super(game, playerID, hackSoTestsWorkWithTypeScript2Dot9Dot1);
     }
 
     prepare() {
