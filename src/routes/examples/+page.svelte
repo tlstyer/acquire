96a73db096b9e0f30dc6a01f6515817d0487698e
Timothy Lee Styer <tlstyer@gmail.com>
2023-02-15 20:11:49 -0800

    PurchaseShares in Svelte

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -2,6 +2,7 @@
   import DisposeOfSharesExamples from './DisposeOfSharesExamples.svelte';
   import GameBoardExamples from './GameBoardExamples.svelte';
   import GameStatusExamples from './GameStatusExamples.svelte';
+  import PurchaseSharesExamples from './PurchaseSharesExamples.svelte';
   import ScoreBoardExamples from './ScoreBoardExamples.svelte';
   import SelectChainExamples from './SelectChainExamples.svelte';
   import TileRackExamples from './TileRackExamples.svelte';
@@ -27,6 +28,7 @@
   <h1>DisposeOfShares</h1>
   <DisposeOfSharesExamples />
   <h1>PurchaseShares</h1>
+  <PurchaseSharesExamples />
   <h1>GameHistory</h1>
   <h1>GameStatus</h1>
   <GameStatusExamples />
