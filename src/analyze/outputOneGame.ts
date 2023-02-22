92d36be7f54934f370bdf447af23f4e10d3a1f37
Timothy Lee Styer <tlstyer@gmail.com>
2023-02-22 15:57:32 -0800

    format files

--- src/analyze/outputOneGame.ts
+++ src/analyze/outputOneGame.ts
@@ -6,36 +6,39 @@ import { getGameHistoryMessageString } from '../common/runGameTestFile';
 import { updateReviewGamePBBinary } from './misc';
 
 function main(gameFilePath: string) {
-  const gameReviewFileContents = fs.readFileSync(gameFilePath);
-  const game = gameFromProtocolBuffer(PB_GameReview.fromBinary(gameReviewFileContents));
+	const gameReviewFileContents = fs.readFileSync(gameFilePath);
+	const game = gameFromProtocolBuffer(PB_GameReview.fromBinary(gameReviewFileContents));
 
-  for (let i = 0; i < game.gameStateHistory.length; i++) {
-    console.log(i);
+	for (let i = 0; i < game.gameStateHistory.length; i++) {
+		console.log(i);
 
-    const gameState = game.gameStateHistory[i];
-    for (const gameHistoryMessage of gameState.gameHistoryMessages) {
-      console.log(getGameHistoryMessageString(gameHistoryMessage));
-    }
-  }
+		const gameState = game.gameStateHistory[i];
+		for (const gameHistoryMessage of gameState.gameHistoryMessages) {
+			console.log(getGameHistoryMessageString(gameHistoryMessage));
+		}
+	}
 
-  console.log();
-  console.log();
-  console.log();
+	console.log();
+	console.log();
+	console.log();
 
-  for (let i = 0; i < game.gameStateHistory.length; i++) {
-    const gameState = game.gameStateHistory[i];
-    const mergedChains = gameState.gameHistoryMessages.filter((m) => m.gameHistoryMessage === GameHistoryMessageEnum.ReceivedBonus).length > 0;
+	for (let i = 0; i < game.gameStateHistory.length; i++) {
+		const gameState = game.gameStateHistory[i];
+		const mergedChains =
+			gameState.gameHistoryMessages.filter(
+				(m) => m.gameHistoryMessage === GameHistoryMessageEnum.ReceivedBonus,
+			).length > 0;
 
-    if (mergedChains) {
-      console.log(i);
+		if (mergedChains) {
+			console.log(i);
 
-      for (const gameHistoryMessage of gameState.gameHistoryMessages) {
-        console.log(getGameHistoryMessageString(gameHistoryMessage));
-      }
-    }
-  }
+			for (const gameHistoryMessage of gameState.gameHistoryMessages) {
+				console.log(getGameHistoryMessageString(gameHistoryMessage));
+			}
+		}
+	}
 
-  updateReviewGamePBBinary(gameReviewFileContents);
+	updateReviewGamePBBinary(gameReviewFileContents);
 }
 
 main(process.argv[2]);
