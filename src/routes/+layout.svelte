87b4958e56337317c342f8a9b47e317b94a4cb4d
Timothy Lee Styer <tlstyer@gmail.com>
2023-03-06 18:49:39 -0800

    move root css to top layout's :root

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,30 @@
 	import './global.css';
 </script>
 
-<div class="root">
-	<Header />
-	<div class="content"><slot /></div>
-</div>
+<Header />
+<div><slot /></div>
 
 <style>
-	.root {
+	:root {
+		background-color: var(--main-background-color);
+		color: #000000;
+		font-family: sans-serif;
+		font-size: 16px;
+
+		--border-color: #f6f4f2;
+
+		--main-background-color-netacquire: #f6f4f2;
+		--main-background-color-white: white;
+		--main-background-color: var(--main-background-color-netacquire);
+
+		--scrolling-div-background-color-netacquire: #c0c0ff;
+		--scrolling-div-background-color-white: white;
+		--scrolling-div-background-color: var(--scrolling-div-background-color-netacquire);
+
 		--header-height: 20px;
 	}
 
-	.content {
+	div {
 		padding-top: var(--header-height);
 	}
 </style>
