078a33819f7cd6968bfea701c2d8c8e98b32e89b
Timothy Lee Styer <tlstyer@gmail.com>
2023-03-21 09:20:23 -0700

    put code in server's main.ts inside a function

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,14 @@
 import { Server } from './server';
 import { WebSocketServerCommunication } from './serverCommunication';
 
-const serverCommunication = new WebSocketServerCommunication();
-const server = new Server(
-	serverCommunication,
-	parseInt(process.env.PUBLIC_VERSION ?? '0', 10),
-	parseInt(process.env.LOG_TIME ?? '0', 10),
-);
-serverCommunication.begin();
+function main() {
+	const serverCommunication = new WebSocketServerCommunication();
+	const server = new Server(
+		serverCommunication,
+		parseInt(process.env.PUBLIC_VERSION ?? '0', 10),
+		parseInt(process.env.LOG_TIME ?? '0', 10),
+	);
+	serverCommunication.begin();
+}
+
+main();
