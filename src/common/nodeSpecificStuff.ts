4b38219b35f7e251413e5ad891c7940cfcdc20bb
Timothy Lee Styer <tlstyer@gmail.com>
2023-03-22 02:44:45 -0700

    delete unused file

--- src/common/nodeSpecificStuff.ts
+++
@@ -1,7 +0,0 @@
-import util from 'util';
-
-export function setupTextDecoderAndTextEncoder() {
-	// @ts-expect-error
-	global.TextDecoder = util.TextDecoder;
-	global.TextEncoder = util.TextEncoder;
-}
