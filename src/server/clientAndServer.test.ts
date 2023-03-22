4ae23c917faf5628083bdcca3b0e32d8591420a5
Timothy Lee Styer <tlstyer@gmail.com>
2023-03-21 19:31:05 -0700

    expect initial message to be correct. deduplicate some.

--- src/server/clientAndServer.test.ts
+++ src/server/clientAndServer.test.ts
@@ -2,20 +2,23 @@ import { Client } from '$lib/client';
 import { TestClientCommunication } from '$lib/clientCommunication';
 import * as libHelpers from '$lib/helpers';
 import { describe, expect, test, vi } from 'vitest';
+import { PB_MessagesToClient } from '../common/pb';
 import { Server } from './server';
 import { TestServerCommunication } from './serverCommunication';
 
-describe('initial message', () => {
+describe('Connect', () => {
 	test('client should reload window when version is different', () => {
 		const spy = vi.spyOn(libHelpers, 'reloadWindow').mockReturnValue();
 
 		const serverCommunication = new TestServerCommunication();
 		new Server(serverCommunication, 2, 123);
 
-		const clientConnection = new TestClientCommunication(serverCommunication);
-		new Client(clientConnection, 1);
+		const clientCommunication = new TestClientCommunication(serverCommunication);
+		new Client(clientCommunication, 1);
 
-		clientConnection.connect();
+		clientCommunication.connect();
+
+		expectInitialMessageToBeCorrect(clientCommunication, serverCommunication);
 
 		expect(spy).toHaveBeenCalledOnce();
 	});
@@ -23,26 +26,54 @@ describe('initial message', () => {
 	test('client should not reload window when version is the same', () => {
 		const spy = vi.spyOn(libHelpers, 'reloadWindow').mockReturnValue();
 
-		const serverCommunication = new TestServerCommunication();
-		new Server(serverCommunication, 2, 123);
+		const { clientCommunication, serverCommunication } = createOneClientConnectedToOneServer();
 
-		const clientConnection = new TestClientCommunication(serverCommunication);
-		new Client(clientConnection, 2);
-
-		clientConnection.connect();
+		expectInitialMessageToBeCorrect(clientCommunication, serverCommunication);
 
 		expect(spy).not.toHaveBeenCalled();
 	});
 
 	test("client's logTime is set to server's logTime", () => {
-		const serverCommunication = new TestServerCommunication();
-		new Server(serverCommunication, 2, 123);
+		const { client, clientCommunication, serverCommunication } =
+			createOneClientConnectedToOneServer();
 
-		const clientConnection = new TestClientCommunication(serverCommunication);
-		const client = new Client(clientConnection, 2);
+		expectInitialMessageToBeCorrect(clientCommunication, serverCommunication);
 
-		clientConnection.connect();
-
-		expect(client.logTime).toEqual(123);
+		expect(client.logTime).toBe(123);
 	});
+
+	function expectInitialMessageToBeCorrect(
+		clientCommunication: TestClientCommunication,
+		serverCommunication: TestServerCommunication,
+	) {
+		const message = PB_MessagesToClient.create({
+			messagesToClient: [
+				{
+					initial: {
+						version: 2,
+						logTime: 123,
+					},
+				},
+			],
+		});
+
+		expect(clientCommunication.communicatedMessages.length).toBe(1);
+		expect(clientCommunication.communicatedMessages[0].receivedMessage).toEqual(message);
+
+		expect(serverCommunication.communicatedMessages.length).toBe(1);
+		expect(serverCommunication.communicatedMessages[0].clientID).toBe(0);
+		expect(serverCommunication.communicatedMessages[0].sentMessage).toEqual(message);
+	}
 });
+
+function createOneClientConnectedToOneServer() {
+	const serverCommunication = new TestServerCommunication();
+	const server = new Server(serverCommunication, 2, 123);
+
+	const clientCommunication = new TestClientCommunication(serverCommunication);
+	const client = new Client(clientCommunication, 2);
+
+	clientCommunication.connect();
+
+	return { client, clientCommunication, server, serverCommunication };
+}
