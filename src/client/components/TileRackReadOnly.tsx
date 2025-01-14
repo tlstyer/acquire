df279c5d77589c2c2a0691e76ec56c6679498f33
Timothy Lee Styer <tlstyer@gmail.com>
2025-01-13 18:03:26 -0800

    make TileRackReadOnly simpler

--- src/client/components/TileRackReadOnly.tsx
+++ src/client/components/TileRackReadOnly.tsx
@@ -1,4 +1,4 @@
-import { createMemo, For, JSX, Match, Switch } from 'solid-js';
+import { createMemo, For, JSX } from 'solid-js';
 import { toTileString } from '../../common/helpers';
 import { PB_GameBoardType } from '../../common/pb';
 import stylesApp from '../App.module.css';
@@ -27,30 +27,17 @@ export function TileRackReadOnly(props: {
     <div class={styles.root} style={{ 'font-size': `${Math.floor(props.buttonSize * 0.4)}px` }}>
       <For each={allTileData}>
         {(tileData) => (
-          <Switch>
-            <Match when={tileData.tile !== null && tileData.type !== null}>
-              <div
-                classList={{
-                  [styles.button]: true,
-                  [gameBoardTypeToCSSClassName.get(tileData.type!)!]: true,
-                }}
-                style={buttonStyle()}
-              >
-                <div>{toTileString(tileData.tile!)}</div>
-              </div>
-            </Match>
-            <Match when={true}>
-              <div
-                classList={{
-                  [styles.button]: true,
-                  [stylesApp.invisible]: true,
-                }}
-                style={buttonStyle()}
-              >
-                ?
-              </div>
-            </Match>
-          </Switch>
+          <div
+            classList={{
+              [styles.button]: true,
+              [tileData.type !== null ? gameBoardTypeToCSSClassName.get(tileData.type)! : '']:
+                tileData.type !== null,
+              [stylesApp.invisible]: tileData.type === null,
+            }}
+            style={buttonStyle()}
+          >
+            <div>{tileData.tile !== null ? toTileString(tileData.tile) : '?'}</div>
+          </div>
         )}
       </For>
     </div>
