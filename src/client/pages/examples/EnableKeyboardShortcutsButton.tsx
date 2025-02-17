0408abd50b719714287cc5b06825c5dbf3da7f01
Timothy Lee Styer <tlstyer@gmail.com>
2025-02-16 23:49:11 -0800

    EnableKeyboardShortcutsButtonContext instead of global variables

--- src/client/pages/examples/EnableKeyboardShortcutsButton.tsx
+++ src/client/pages/examples/EnableKeyboardShortcutsButton.tsx
@@ -1,23 +1,49 @@
-import { createEffect, createSelector, createSignal } from 'solid-js';
-
-let nextId = 0;
-const [onlyEnabledId, setOnlyEnabledId] = createSignal(nextId++);
-const isIdEnabled = createSelector(onlyEnabledId);
+import { createContext, createEffect, createSelector, createSignal, useContext } from 'solid-js';
 
 export function EnableKeyboardShortcutsButton(props: {
   onChangeEnabled: (enabled: boolean) => void;
 }) {
-  const myId = nextId++;
+  const context = useEnableKeyboardShortcutsButtonContext();
+
+  if (!context) {
+    throw new Error('missing context');
+  }
+
+  const myId = context.nextId();
 
   createEffect(() => {
-    props.onChangeEnabled(isIdEnabled(myId));
+    props.onChangeEnabled(context.isIdEnabled(myId));
   });
 
   return (
     <input
       type="button"
-      value={isIdEnabled(myId) ? 'Keyboard Shortcuts Enabled' : 'Enable Keyboard Shortcuts'}
-      onClick={() => setOnlyEnabledId((id) => (id === myId ? 0 : myId))}
+      value={context.isIdEnabled(myId) ? 'Keyboard Shortcuts Enabled' : 'Enable Keyboard Shortcuts'}
+      onClick={() => context.setOnlyEnabledId((id) => (id === myId ? 0 : myId))}
     />
   );
 }
+
+export function makeEnableKeyboardShortcutsButtonContext() {
+  let nextId = 0;
+  const [onlyEnabledId, setOnlyEnabledId] = createSignal(nextId++);
+  const isIdEnabled = createSelector(onlyEnabledId);
+
+  return {
+    nextId() {
+      return nextId++;
+    },
+    setOnlyEnabledId,
+    isIdEnabled,
+  };
+}
+
+type EnableKeyboardShortcutsButtonContextType = ReturnType<
+  typeof makeEnableKeyboardShortcutsButtonContext
+>;
+
+export const EnableKeyboardShortcutsButtonContext =
+  createContext<EnableKeyboardShortcutsButtonContextType>();
+
+const useEnableKeyboardShortcutsButtonContext = () =>
+  useContext(EnableKeyboardShortcutsButtonContext);
--- src/client/pages/examples/ExamplesPage.tsx
+++ src/client/pages/examples/ExamplesPage.tsx
@@ -1,5 +1,9 @@
 import { CreateGameExamples } from './CreateGameExamples.js';
 import { DisposeOfSharesExamples } from './DisposeOfSharesExamples.js';
+import {
+  EnableKeyboardShortcutsButtonContext,
+  makeEnableKeyboardShortcutsButtonContext,
+} from './EnableKeyboardShortcutsButton.jsx';
 import styles from './ExamplesPage.module.css';
 import { GameBoardExamples } from './GameBoardExamples.js';
 import { GameHistoryExamples } from './GameHistoryExamples.js';
@@ -14,31 +18,35 @@ import { TileRackReadOnlyExamples } from './TileRackReadOnlyExamples.js';
 
 export function ExamplesPage() {
   return (
-    <div class={styles.root}>
-      <h1>CreateGame</h1>
-      <CreateGameExamples />
-      <h1>GameListing</h1>
-      <GameListingExamples />
-      <h1>GameSetupUI</h1>
-      <GameSetupUIExamples />
-      <h1>GameBoard</h1>
-      <GameBoardExamples />
-      <h1>ScoreBoard</h1>
-      <ScoreBoardExamples />
-      <h1>TileRack</h1>
-      <TileRackExamples />
-      <h1>TileRackReadOnly</h1>
-      <TileRackReadOnlyExamples />
-      <h1>SelectChain</h1>
-      <SelectChainExamples />
-      <h1>DisposeOfShares</h1>
-      <DisposeOfSharesExamples />
-      <h1>PurchaseShares</h1>
-      <PurchaseSharesExamples />
-      <h1>GameHistory</h1>
-      <GameHistoryExamples />
-      <h1>NextGameAction</h1>
-      <NextGameActionExamples />
-    </div>
+    <EnableKeyboardShortcutsButtonContext.Provider
+      value={makeEnableKeyboardShortcutsButtonContext()}
+    >
+      <div class={styles.root}>
+        <h1>CreateGame</h1>
+        <CreateGameExamples />
+        <h1>GameListing</h1>
+        <GameListingExamples />
+        <h1>GameSetupUI</h1>
+        <GameSetupUIExamples />
+        <h1>GameBoard</h1>
+        <GameBoardExamples />
+        <h1>ScoreBoard</h1>
+        <ScoreBoardExamples />
+        <h1>TileRack</h1>
+        <TileRackExamples />
+        <h1>TileRackReadOnly</h1>
+        <TileRackReadOnlyExamples />
+        <h1>SelectChain</h1>
+        <SelectChainExamples />
+        <h1>DisposeOfShares</h1>
+        <DisposeOfSharesExamples />
+        <h1>PurchaseShares</h1>
+        <PurchaseSharesExamples />
+        <h1>GameHistory</h1>
+        <GameHistoryExamples />
+        <h1>NextGameAction</h1>
+        <NextGameActionExamples />
+      </div>
+    </EnableKeyboardShortcutsButtonContext.Provider>
   );
 }
