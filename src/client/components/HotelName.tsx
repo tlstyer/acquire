f3a8212a0a24f9cab3b63186602c25171489bf5b
Timothy Lee Styer <tlstyer@gmail.com>
2025-03-04 11:34:42 -0800

    make Username and HotelName components look nice in Firefox

--- src/client/components/HotelName.tsx
+++ src/client/components/HotelName.tsx
@@ -1,10 +1,16 @@
 import { type PB_GameBoardType } from '../../common/pb.js';
 import { gameBoardTypeToHotelName } from '../helpers.js';
 import { gameBoardTypeToCSSClassName } from '../styleHelpers.js';
+import styles from './HotelName.module.css';
 
 export function HotelName(props: { chain: PB_GameBoardType }) {
   return (
-    <span class={gameBoardTypeToCSSClassName.get(props.chain)}>
+    <span
+      classList={{
+        [styles.root]: true,
+        [gameBoardTypeToCSSClassName.get(props.chain)!]: true,
+      }}
+    >
       {gameBoardTypeToHotelName.get(props.chain)}
     </span>
   );
